6c599d2fa22e85e657e3273b7a5c697c242fac0c
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2023-05-05 17:17:04 +0530

    assets folder setup and app images added

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String chatLogo = "assets/images/chat_logo.png";
+  static const String openaiLogo = "assets/images/openai_logo.png";
+  static const String personImg = "assets/images/person.jpg";
+}
+
+
